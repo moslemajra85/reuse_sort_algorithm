46f5a94f51a9597f06f195dbaa211799112311ab
Moslem Ajra <ajra.engineering@gmail.Com>
2023-11-10 02:38:42 +0100

    Delegate the operations of comparing and swaping to an appropriate type of object

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,8 @@
-class Sorter {
-  constructor(public collection: number[]) {}
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([12, -1, 0, -4, 7]);
+const numbersCollection = new NumbersCollection([12, -1, 0, -4, 7]);
+const sorter = new Sorter(numbersCollection);
 
 sorter.sort();
-console.log(sorter.collection);
+console.log(sorter.collection.data);
